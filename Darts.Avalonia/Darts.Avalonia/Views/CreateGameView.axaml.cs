854f870a7caadecdabc2165fbbb25f181bf8bd9b
Juraj Sokol <jurajsokol.s@gmail.com>
2024-11-24 11:15:01 +0100

    Add change theme button

--- Darts.Avalonia/Darts.Avalonia/Views/CreateGameView.axaml.cs
+++ Darts.Avalonia/Darts.Avalonia/Views/CreateGameView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 using Darts.Avalonia.ViewModels;
 
 namespace Darts.Avalonia.Views;
@@ -15,7 +17,16 @@ public partial class CreateGameView : UserControl
         Loaded += async (_, _) => await ViewModel.LoadPlayers();
     }
 
-    public CreateGameView()
-    { 
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        string actualThemeVariant = (string)App.Current.ActualThemeVariant.Key;
+        if (actualThemeVariant == "Light")
+        {
+            App.Current.RequestedThemeVariant = new ThemeVariant("Dark", null);
+        }
+        else
+        {
+            App.Current.RequestedThemeVariant = new ThemeVariant("Light", null);
+        }
     }
 }
